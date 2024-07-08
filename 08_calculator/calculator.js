04c0cf29b85845a68a38e7c9107fb8acf4c50e5c
SkeleDrook <hulik.ondrej@gmail.com>
2024-07-08 21:09:11 +0000

    Finish Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,33 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  if (arr === undefined || arr.length == 0) {
+    return 0;
+  }
+  return arr.reduce((previous, current) => previous += current);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((previous, current) => previous *= current);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let result = 1; // Start with 1, the multiplicative identity
+  for (let i = n; i > 1; i--) { // Loop from n down to 2
+    result *= i;
+  }
+  return result;
 };
-
 // Do not edit below this line
 module.exports = {
   add,
